932030b15a603d57d1c22137f83086e1b456528f
rshipp <github@rshipp.com>
2016-02-16 17:36:49 -0700

    Implement get_cpu_info

--- src/info/cpu_info.cpp
+++ src/info/cpu_info.cpp
@@ -2,13 +2,43 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <stdlib.h>
 
 using namespace std;
 
 
 vector<CpuInfo> get_cpu_info() {
-  // TODO: implement me
-  return vector<CpuInfo>();
+  ifstream stat_file(PROC_ROOT "/stat");
+  vector<CpuInfo> cpu_info;
+  string line;
+
+  if (!stat_file) {
+    cerr << "Unable to read from /proc/stat" << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  while (getline(stat_file, line)) {
+    if(line.find("cpu") == 0) {
+      CpuInfo cpu;
+      // Strip off the first "cpuX" characters and make a stream.
+      istringstream iss(line.substr(4, line.size()));
+      iss
+          >> cpu.user_time
+          >> cpu.nice_time
+          >> cpu.system_time
+          >> cpu.idle_time
+          >> cpu.io_wait_time
+          >> cpu.irq_time
+          >> cpu.softirq_time
+          >> cpu.steal_time
+          >> cpu.guest_time
+          >> cpu.guest_nice_time;
+      cpu_info.insert(cpu_info.end(), cpu);
+    }
+  }
+
+  return cpu_info;
 }
 
 
