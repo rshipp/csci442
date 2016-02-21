40a0cf067d814d630326f999fd5fdfa89bc11c9d
rshipp <github@rshipp.com>
2016-02-21 15:11:41 -0700

    Add formatting.{h,cpp}

---
+++ src/utils/formatting.cpp
@@ -0,0 +1,15 @@
+#include "formatting.h"
+
+using namespace std;
+
+string humanize_bytes(int bytes) {
+  return string();
+}
+
+string humanize_time(int ticks) {
+  return string();
+}
+
+string humanize_uptime(float seconds) {
+  return string();
+}
---
+++ src/utils/formatting.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+
+std::string humanize_bytes(int bytes);
+
+std::string humanize_time(int ticks);
+
+std::string humanize_uptime(float seconds);
