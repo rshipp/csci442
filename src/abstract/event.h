ce14a96f154e7e39a09b7a293977a51a57244784
rshipp <github@rshipp.com>
2016-03-22 18:26:00 -0600

    Fix compile error

--- src/abstract/event.h
+++ src/abstract/event.h
@@ -91,33 +91,4 @@ struct EventComparator {
   }
 };
 
-std::string event_type_str(Event::EventType num) {
-  std::string type;
-  switch(num) {
-      case Event::THREAD_ARRIVED:
-          type = "THREAD_ARRIVED";
-          break;
-      case Event::THREAD_DISPATCH_COMPLETED:
-          type = "THREAD_DISPATCH_COMPLETED";
-          break;
-      case Event::PROCESS_DISPATCH_COMPLETED:
-          type = "PROCESS_DISPATCH_COMPLETED";
-          break;
-      case Event::CPU_BURST_COMPLETED:
-          type = "CPU_BURST_COMPLETED";
-          break;
-      case Event::IO_BURST_COMPLETED:
-          type = "IO_BURST_COMPLETED";
-          break;
-      case Event::THREAD_COMPLETED:
-          type = "THREAD_COMPLETED";
-          break;
-      case Event::THREAD_PREEMPTED:
-          type = "THREAD_PREEMPTED";
-          break;
-      case Event::DISPATCHER_INVOKED:
-          type = "DISPATCHER_INVOKED";
-          break;
-  }
-  return type;
-}
+std::string event_type_str(Event::EventType num);
