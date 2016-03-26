2c6eec9cca56ad21599c65f15dedc61d81c8ca1d
rshipp <github@rshipp.com>
2016-03-26 11:43:54 -0600

    Implememnt round robin

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
     }
   }
 
-  Scheduler* scheduler = new FcfsScheduler();
+  Scheduler* scheduler = new RoundRobinScheduler();
   Logger* logger = new Logger(false, false);
   Simulation* simulation = new Simulation(scheduler, *logger, config.thread_switch_overhead, config.process_switch_overhead);
   simulation->run(events);
