8fd185ff22b4d1a196ba77b06b8be9c35ef0da29
rshipp <github@rshipp.com>
2016-03-26 11:25:54 -0600

    Remove old code

--- src/main.cpp
+++ src/main.cpp
@@ -33,21 +33,5 @@ int main(int argc, char** argv) {
   Simulation* simulation = new Simulation(scheduler, *logger, config.thread_switch_overhead, config.process_switch_overhead);
   simulation->run(events);
 
-  // Print the queue
-  // NOTE: The queue is destroyed at this time, so do this last.
-  while (!events.empty()) {
-    // Get the next event (the one that happens next) from the 'top' of the queue.
-    const Event* event = events.top();
-
-    // The top() method just returns the element, it doesn't remove it. You have
-    // to call pop() to do that.
-    events.pop();
-
-    // Process the event using the appropriate logic. I use a switch statement
-    // based on the type of the event. When handling an event, you might add
-    // additional events to the queue. For example, if you encounter a
-    // THREAD_COMPLETED event, you might add a new one for DISPATCHER_INVOKED.
-    
-  }
   return 0;
 }
