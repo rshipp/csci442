90294c2728d07d61eedb3ae36279bc55a80fe4c3
rshipp <github@rshipp.com>
2016-04-12 19:24:48 -0600

    Implement VirtualAddress

--- src/virtual_address/virtual_address.cpp
+++ src/virtual_address/virtual_address.cpp
@@ -5,23 +5,33 @@
  */
 
 #include "virtual_address/virtual_address.h"
+#include <bitset>
+#include <iostream>
 
 using namespace std;
 
 
 VirtualAddress VirtualAddress::from_string(int process_id, string address) {
-  // TODO: implement me
-  return VirtualAddress(0, 0, 0);
+  size_t addr = bitset<ADDRESS_BITS>(address).to_ulong();
+  size_t page = addr >> OFFSET_BITS;
+  size_t offset = addr - (page << OFFSET_BITS);
+
+  return VirtualAddress(process_id, page, offset);
 }
 
 
 string VirtualAddress::to_string() const {
-  // TODO: implement me
-  return "";
+  int address = (page << OFFSET_BITS) + offset;
+  return bitset<ADDRESS_BITS>(address).to_string();
 }
 
 
 ostream& operator <<(ostream& out, const VirtualAddress& address) {
-  // TODO: implement me
+  out
+      << "PID " << address.process_id
+      << " @ " << address.to_string()
+      << " [page: " << address.page
+      << "; offset: " << address.offset
+      << "]";
   return out;
 }
