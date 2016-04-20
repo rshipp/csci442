0d660c1cf7f6bd4bf27bf71fb2cbeaf699f703ae
rshipp <github@rshipp.com>
2016-04-19 19:57:37 -0600

    Implement FlagParser

--- src/flag_parser/flag_parser.cpp
+++ src/flag_parser/flag_parser.cpp
@@ -6,6 +6,9 @@
 
 #include "flag_parser/flag_parser.h"
 #include <iostream>
+#include <stdio.h>     /* for printf */
+#include <stdlib.h>    /* for exit */
+#include <getopt.h>
 
 using namespace std;
 
@@ -31,6 +34,70 @@ void print_usage() {
 
 
 bool parse_flags(int argc, char** argv, FlagOptions& flags) {
-  // TODO: implement me
-  return false;
+  int c;
+
+  string sarg;
+  int iarg;
+
+  while (1) {
+     int option_index = 0;
+     static struct option long_options[] = {
+         {"verbose",    no_argument,       0, 'v' },
+         {"strategy",   required_argument, 0, 's' },
+         {"max-frames", required_argument, 0, 'f' },
+         {"help",       no_argument,       0, 'h' },
+         {0,            0,                 0,  0 }
+     };
+
+
+     c = getopt_long(argc, argv, "vs:f:h",
+              long_options, &option_index);
+     if (c == -1)
+         break;
+
+     switch (c) {
+     case 'h':
+         print_usage();
+         break;
+
+     case 'v':
+         flags.verbose = true;
+         break;
+
+     case 's':
+         sarg = string(optarg);
+         if(sarg == "FIFO" || sarg == "LRU") {
+           if(sarg == "LRU") {
+             flags.strategy = ReplacementStrategy::LRU;
+           }
+         } else {
+           printf("Valid values are FIFO, LRU\n");
+           return false;
+         }
+         break;
+
+     case 'f':
+         iarg = atoi(optarg);
+         if(iarg > 1) {
+           flags.max_frames = iarg;
+         } else {
+           printf("Must be a positive integer\n");
+           return false;
+         }
+         break;
+
+     case '?':
+         return false;
+     }
+  }
+
+  if (optind < argc) {
+    flags.filename = string(argv[argc-1]);
+  } else {
+    // Missing filename
+    printf("Missing filename\n");
+    return false;
+  }
+
+  return true;
 }
