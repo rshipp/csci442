b62700fad6f5889557b416507883cd4fcc595c38
rshipp <github@rshipp.com>
2016-04-19 19:15:04 -0600

    Implement PageTable

--- src/page_table/page_table.cpp
+++ src/page_table/page_table.cpp
@@ -10,18 +10,35 @@ using namespace std;
 
 
 size_t PageTable::get_present_page_count() const {
-  // TODO: implement me
-  return 0;
+  size_t sum = 0;
+  for (size_t i=0; i < rows.size(); i++) {
+    if (rows[i].present) {
+      sum++;
+    }
+  }
+  return sum;
 }
 
 
 size_t PageTable::get_oldest_page() const {
-  // TODO: implement me
-  return 0;
+  size_t page = -1, loaded = -1;
+  for (size_t i = 0; i < rows.size(); i++) {
+    if (rows[i].present && rows[i].loaded_at < loaded) {
+      loaded = rows[i].loaded_at;
+      page = i;
+    }
+  }
+  return page;
 }
 
 
 size_t PageTable::get_least_recently_used_page() const {
-  // TODO: implement me
-  return 0;
+  size_t page = -1, used = -1;
+  for (size_t i = 0; i < rows.size(); i++) {
+    if (rows[i].present && rows[i].last_accessed_at < used) {
+      used = rows[i].last_accessed_at;
+      page = i;
+    }
+  }
+  return page;
 }
